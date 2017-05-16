0e5de2479eb55891ae06dc1dea910c12b92cc82e
bioshopik <bioshopik@gmail.com>
2017-05-16 18:45:10 +0300

    first edition

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  document.querySelector('.search-form .keyword')
+          .value = 'Keyword, name, date, ...';
+  
+  document.querySelector('.email-form .email')
+          .value = 'your@email.com';
+}
+
+var nav = document.querySelector('#header .main-nav');
+
+var burger = document.querySelector('#header .burger');
+burger.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  nav.classList.add('show');
+});
+
+
+var close = document.querySelector('#header .close');
+close.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  nav.classList.remove('show');
+});
